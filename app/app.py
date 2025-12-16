89c465e4656401b69eb483639573a438bf2a6f3c
El-pabs <robin.gillard1@std.heh.be>
2025-12-16 14:52:51 +0100

    add

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     # Affiche le nom du serveur (Pod) pour prouver que ça tourne
-    return f"<h1>Hello depuis AWS EC2 !</h1><p>Servi par le pod: {socket.gethostname()}</p>"
+    return f"<h1>Hello depuis AWS EC2 ! test 1</h1><p>Servi par le pod: {socket.gethostname()}</p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
