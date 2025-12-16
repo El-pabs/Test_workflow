deea12bd7bb9a79f9f83ecd41c4b6060b5bff561
El-pabs <robin.gillard1@std.heh.be>
2025-12-16 13:11:02 +0100

    add

---
+++ app/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    # Affiche le nom du serveur (Pod) pour prouver que ça tourne
+    return f"<h1>Hello depuis AWS EC2 !</h1><p>Servi par le pod: {socket.gethostname()}</p>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
